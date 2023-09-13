51d245b0bfba37341d2d69767a68c25c1a7e62fb
ugurakcora <uakcora97@gmail.com>
2023-09-13 15:48:08 +0300

    init react icons

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="">
+      <div>MovieApp</div>
+      <div>
+        <input type="text"></input>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
